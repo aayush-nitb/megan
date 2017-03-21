b18e19cc90b995814e22cefc0a4a41991f508b00
aaagrawa <aaagrawa@adobe.com>
2017-03-21 14:34:44 +0530

    get port from env

--- server/api/Api_Module.ts
+++ server/api/Api_Module.ts
@@ -1,20 +1,21 @@
 import * as express from 'express'
 import { Server } from 'typescript-rest'
 
-require('./v1/Api_Courses');
+//require('./v1/Api_Courses');
 
 export abstract class Api_Module {
     static serve() {
         let app: express.Application = express();
         let mongoose = require('mongoose');
+        let port = process.env.PORT || 3000;
         //mongoose.connect(process.env.MONGODB_URL);
         //Server.setFileDest('/uploads');
         //Server.buildServices(app);
         app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
             res.json(req);
         });
-        app.listen(3000, function() {
-            console.log('Rest Server listening on port 3000!');
+        app.listen(port, function() {
+            console.log('Rest Server listening on port ' + port);
         });
     }
 }
