a12965e46714034389131bc385b4434734f3ec28
aaagrawa <aaagrawa@adobe.com>
2017-03-21 14:38:20 +0530

    enable routes and db

--- server/api/Api_Module.ts
+++ server/api/Api_Module.ts
@@ -1,19 +1,16 @@
 import * as express from 'express'
 import { Server } from 'typescript-rest'
 
-//require('./v1/Api_Courses');
+require('./v1/Api_Courses');
 
 export abstract class Api_Module {
     static serve() {
         let app: express.Application = express();
         let mongoose = require('mongoose');
         let port = process.env.PORT || 3000;
-        //mongoose.connect(process.env.MONGODB_URL);
-        //Server.setFileDest('/uploads');
-        //Server.buildServices(app);
-        app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-            res.json(req);
-        });
+        mongoose.connect(process.env.MONGODB_URL);
+        Server.setFileDest('/uploads');
+        Server.buildServices(app);
         app.listen(port, function() {
             console.log('Rest Server listening on port ' + port);
         });
