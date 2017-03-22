533531882d66bb08b0a41f7f5b39b02b35770254
Aayush Agrawal <aaagrawa@adobe.com>
2017-03-22 23:32:06 +0530

    apply cors

--- server/api/Api_Module.ts
+++ server/api/Api_Module.ts
@@ -1,4 +1,5 @@
 import * as express from 'express'
+import * as cors  from 'cors'
 import { Server } from 'typescript-rest'
 
 require('./v1/Api_Courses');
@@ -9,6 +10,7 @@ export abstract class Api_Module {
         let mongoose = require('mongoose');
         let port = process.env.PORT || 3000;
         mongoose.connect(process.env.MONGODB_URL);
+        app.use(cors());
         Server.setFileDest('/uploads');
         Server.buildServices(app);
         app.listen(port, function() {
